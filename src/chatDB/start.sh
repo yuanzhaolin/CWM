a02eee4fe42d775737ec1465c81beb4533befefd
yuanzhaolin <18810919727@163.com>
2024-11-05 23:07:49 +0800

    update python path

--- src/chatDB/start.sh
+++ src/chatDB/start.sh
@@ -1,4 +1,4 @@
 ps -ef | grep chatdb-api.py | grep -v grep | awk '{print $2}' | xargs kill
-python_path='/home/fashion/miniconda3/envs/chatdb/bin/python'
+python_path='/data/yuanzhaolin/LLM_garment_MES/env/bin/python'
 nohup $python_path -u chatdb-api.py >> chatdb-api.log 2>&1 &
 
